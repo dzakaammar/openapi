06d9f091079b6c22d567fe06cf4eed0b093ebcf9
Dzaka Ammar Ibrahim <dzakaammar@gmail.com>
2023-11-05 11:55:51 +0700

    init

---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen -config=./spec/config.yml ./spec/api.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen -config=./spec/config2.yml ./spec/api2.yaml
+func main() {
+}
